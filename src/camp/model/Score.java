7bdf0fd47ffb0a8bd73ac318a7b51bfe22d280df
kissco_ejchae <ejchae@kissco.co.jp>
2024-01-11 12:43:51 +0900

    model / 필드 추가

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -1,7 +1,13 @@
 package camp.model;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Score {
     private String scoreId;
+    private String studentId;
+    private String subjectId;
+    private ArrayList<String> gradeList;
 
     public Score(String seq) {
         this.scoreId = seq;
--- src/camp/model/Student.java
+++ src/camp/model/Student.java
@@ -1,12 +1,15 @@
 package camp.model;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
 public class Student {
     private String studentId;
     private String studentName;
     private List<Subject> enrolledMandatorySubjects; // 수강생 등록시, 수강생이 신청한 필수 과목을 담음
     private List<Subject> enrolledOptionalSubjects; // 이건 선택 옵션
-    private List<Score> scores; // 등록되는 수강생의 과목 점수
+    private List<Score> scores;
 
     public Student(String seq, String studentName) {
         this.studentId = seq;
@@ -21,21 +24,27 @@ public class Student {
     public String getStudentName() {
         return studentName;
     }
+
     public List<Subject> getEnrolledMandatorySubjects() {
         return enrolledMandatorySubjects;
     }
+
     public List<Subject> getEnrolledOptionalSubjects() {
         return enrolledOptionalSubjects;
     }
+
     public List<Score> getScores() {
         return scores;
     }
+
     public void enrollMandatorySubject(Subject subject) { // 입력받은 수강생의 필수 과목 저장
         enrolledMandatorySubjects.add(subject);
     }
+
     public void enrollOptionalSubject(Subject subject) { // 입력받은 수강생의 선택 과목 저장
         enrolledOptionalSubjects.add(subject);
     }
+
     public void addScore(Score score) { // 입력받은 수강생의 과목 점수 저장
         scores.add(score);
     }
