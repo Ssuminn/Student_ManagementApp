35b50e0be08682575bcff7fe63d38b5072df644b
LeeJaeHyung <leejh7623@gmail.com>
2024-01-16 14:41:14 +0900

    점수관리 항목 누락 수정

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -409,30 +409,34 @@ public class CampManagementApplication {
         return null;
     }
 
-    private static void displayScoreView() throws Exception {
-        boolean flag = true;
-        while (flag) {
-            System.out.println("==================================");
-            System.out.println("점수 관리 실행 중...");
-            System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
-            System.out.println("2. 수강생의 과목별 회차 점수 수정");
-            System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
-            System.out.println("4. 메인 화면 이동");
-            System.out.print("관리 항목을 선택하세요...");
-            int input = sc.nextInt();
+	private static void displayScoreView() throws Exception {
+		boolean flag = true;
+		while (flag) {
+			System.out.println("==================================");
+			System.out.println("점수 관리 실행 중...");
+			System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
+			System.out.println("2. 수강생의 과목별 회차 점수 수정");
+			System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
+			System.out.println("4. 수강생의 과목별 평균 등급을 조회");
+			System.out.println("5. 특정 상태 수강생들의 필수 과목 평균 등급을 조회");
+			System.out.println("6. 메인 화면 이동");
+			System.out.print("관리 항목을 선택하세요...");
+			int input = sc.nextInt();
+			switch (input) {
+				case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
+				case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
+				case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
+				case 4 -> inquireEvgGradeBySubject(); // 수강생의 과목별 평균 등급을 조회
+				case 5 -> inquireEvgGradeByMandatorySubject(); // 특정 상태 수강생들의 필수 과목 평균 등급을 조회
+				case 6 -> flag = false; // 메인 화면 이동
+				default -> {
+					System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
+					flag = false;
+				}
+			}
+		}
+	}
 
-            switch (input) {
-                case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
-                case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
-                case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
-                case 4 -> flag = false; // 메인 화면 이동
-                default -> {
-                    System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
-                    flag = false;
-                }
-            }
-        }
-    }
 
 	// 수강생의 과목별 시험 회차 및 점수 등록
 	private static void createScore() throws Exception {
